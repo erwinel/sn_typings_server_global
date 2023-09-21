e2876bb0564ec4f4f0e5b8035fec592f59f799f3
erwinel <37079914+erwinel@users.noreply.github.com>
2023-09-21 13:38:53 +0000

    Removed obsolete files

--- dist/types/UsmsProgramOpsUtil_Tests.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../types/index.d.ts" />
-/// <reference path="../../types/sn_atf.d.ts" />
-/// <reference path="../../types/jasmine.d.ts" />
-/// <reference path="../../src/x_g_doj_usmsprogop.d.ts" />
-/// <reference path="../../src/UsmsProgramOpsUtil.d.ts" />
-declare namespace UsmsProgramOpsUtil_Tests {
-}
